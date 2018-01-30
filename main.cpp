eb2e43c90c02858d28c4304ff7474202a40c0ceb
Martien Oppeneer <martien@oppeneer.com>
2018-01-30 21:06:32 +0100

    Initial code to use opengl and glfw.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <thread>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+
+    glfwInit();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    glfwTerminate();
+
     return 0;
 }
