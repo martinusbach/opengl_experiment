c69fb714e916978d8f094bcaae02df9a35685392
Martien Oppeneer <martien@oppeneer.com>
2018-01-31 22:15:27 +0100

    Added glbinding library for loading OpenGL extensions.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <GL/gl.h>
+
+#include <glbinding/gl/gl.h>
+#include <glbinding/Binding.h>
 #include <GLFW/glfw3.h>
 
 
 using namespace std;
+using namespace gl;
+
 
 int main()
 {
     cout << "Hello World!" << endl;
 
+    // try creating a windowed opengl context with glfw
     glfwInit();
-
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -23,12 +27,22 @@ int main()
 
     glfwMakeContextCurrent(window);
 
+    // try out opengl binding
+    glbinding::Binding::initialize();
+
+    GLuint vertexBuffer = 0;
+    glGenBuffers(1, &vertexBuffer);
+
+    std::cout << "vertex buffer id: " << vertexBuffer << " (should be 1)" << std::endl;
+
+    // main event loop
     while(!glfwWindowShouldClose(window))
     {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // Done!
     glfwTerminate();
 
     return 0;
