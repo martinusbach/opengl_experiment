111c3c00b2fdcfca6d167dba2597c193f58b5cbb
Martien Oppeneer <martien@oppeneer.com>
2018-02-04 22:29:55 +0100

    Cleanup, no functional changes.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 
 #include <glbinding/gl/gl.h>
-#include <glbinding/gl/functions-patches.h>  // required to allow non-type-safe functions calls
 #include <glbinding/Binding.h>
 #include <GLFW/glfw3.h>
 
@@ -11,23 +10,19 @@ using namespace std;
 using namespace gl;
 
 
-
 int main()
 {
     cout << "Hello World!" << endl;
 
-    // try creating a windowed opengl context with glfw
     glfwInit();
 
+    // Create a windowed OpenGL context with GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
-
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
 
@@ -41,23 +36,29 @@ int main()
     // Create a Vertex Buffer Object and copy the vertex data to it
     GLuint vbo;
     glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
     GLfloat vertices[] = {
-         0.0f,  0.5f,
-         0.5f, -0.5f,
-        -0.5f, -0.5f
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
     };
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);  // make vbo the 'active' buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  // copy to 'active' buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Shader sources
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     const GLchar* vertexSource = R"glsl(
         #version 150 core
-        in vec2 position;
+        in vec3 position;
         void main()
         {
-            gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = vec4(position.x, position.y, position.z, 1.0);
         }
     )glsl";
+    glShaderSource(vertexShader, 1, &vertexSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     const GLchar* fragmentSource = R"glsl(
         #version 150 core
         uniform vec3 triangleColor;
@@ -67,15 +68,7 @@ int main()
             outColor = vec4(triangleColor, 1.0);
         }
     )glsl";
-
-    // Create and compile the vertex shader
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
-
-    // Create and compile the fragment shader
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
     glCompileShader(fragmentShader);
 
     // Link the vertex and fragment shader into a shader program
@@ -89,7 +82,7 @@ int main()
     // Specify the layout of the vertex data
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glEnableVertexAttribArray(static_cast<GLuint>(posAttrib));
-    glVertexAttribPointer(static_cast<GLuint>(posAttrib), 2, static_cast<gl::GLenum>(GL_FLOAT), GL_FALSE, 0, 0);
+    glVertexAttribPointer(static_cast<GLuint>(posAttrib), 3, static_cast<gl::GLenum>(GL_FLOAT), GL_FALSE, 0, 0);
 
     // main event loop
     auto t_start = std::chrono::high_resolution_clock::now();
@@ -118,9 +111,7 @@ int main()
     glDeleteProgram(shaderProgram);
     glDeleteShader(fragmentShader);
     glDeleteShader(vertexShader);
-
     glDeleteBuffers(1, &vbo);
-
     glDeleteVertexArrays(1, &vao);
 
     glfwTerminate();
